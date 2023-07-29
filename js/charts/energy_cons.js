3148207c6898f305a2d747634129f0a5468828d1
CodingWithJS <learningtocodejs@gmail.com>
2023-07-29 15:48:34 +0530

    Improved charts

--- js/charts/energy_cons.js
+++ js/charts/energy_cons.js
@@ -1,7 +1,7 @@
-async function init_energy_cons(svg_width, svg_height, start_year=1800, end_year=2022, svg_id='#global-warming-chart') {
+async function init_energy_cons(svg_width, svg_height, start_year = 1800, end_year = 2022, svg_id = '#global-warming-chart') {
     const width = svg_width;
     const height = svg_height;
-    const margin = { top: 20, right: 100, bottom: 40, left: 60 };
+    const margin = { top: 20, right: 20, bottom: 40, left: 60 };
     const chartWidth = width - margin.left - margin.right;
     const chartHeight = height - margin.top - margin.bottom;
     const default_start_year = 1800;
@@ -11,12 +11,23 @@ async function init_energy_cons(svg_width, svg_height, start_year=1800, end_year
     let data = await d3.csv("././data/global-energy-substitution.csv")
     // Parse the data into appropriate types
     //data = data.filter(d => Number(d.year) >= start_year).filter(d => Number(d.year) <= end_year);
-    
+
     data.forEach(d => {
         d.year = Number(d.year);
-        d.coal = Number(d.coal);
     });
-    console.log('Data: ',data)
+    console.log('Data: ', data, data.columns, data['coal']);
+
+    var source = data.columns.slice(3).map(function (id) {
+        return {
+            id: id,
+            values: data.map(function (d) {
+                return { year: d.year, energy: d[id] };
+            })
+        };
+    });
+
+    console.log(source);
+
 
     /*if (start_year !== default_start_year && end_year !== default_end_year) {
         console.log("Please reset chart!")
@@ -31,55 +42,68 @@ async function init_energy_cons(svg_width, svg_height, start_year=1800, end_year
 
     // Create the SVG element
     const svg = d3
-    .select(svg_id)
-    .append('g')
-    .attr('width', width)
-    .attr('height', height);
+        .select(svg_id)
+        .append('g')
+        .attr('width', width)
+        .attr('height', height);
 
     const transitionPath = d3
-    .transition()
-    .duration(2500)
-    .ease(d3.easeSin);
+        .transition()
+        .duration(2500)
+        .ease(d3.easeSin);
 
     // Create a scale for the x-axis
     const xScale = d3
-    .scaleLinear()
-    .domain([start_year, end_year])
-    .range([0, chartWidth])
+        .scaleLinear()
+        .domain([start_year, end_year])
+        .range([0, chartWidth])
 
     // Create a scale for the y-axis
     const yScale = d3
-    .scaleLinear()
-    .domain([0, 60000])
-    .range([chartHeight, 0]);
+        .scaleLinear()
+        .domain([0, 60000])
+        .range([chartHeight, 0]);
 
-    // Create the line generator
-    const line_coal = d3
-    .line()
-    .x(d => margin.left + xScale(d.year))
-    .y(d => margin.top + yScale(d.coal));
+    // color palette
+    var res = source.map(function (d) { return d.id }) // list of group names
+    console.log(res);
+    var color = d3.scaleOrdinal()
+        .domain(res)
+        .range(['#e41a1c', '#377eb8', '#4daf4a', '#984ea3', '#ff7f00', '#ffff33', '#a65628', '#f781bf', '#999999', '#000fff'])
 
-    const line_oil = d3
-    .line()
-    .x(d => margin.left + xScale(d.year))
-    .y(d => margin.top + yScale(d.oil));
 
 
     // Append the line path
-    svg.append('path')
-        .datum(data)
-        .attr('d', line_coal)
-        .attr('fill', 'none')
-        .transition(transitionPath)
-        .attr('stroke', 'red')
-        .attr('stroke-width', 2);
+    const path = svg.selectAll(".line")
+        .data(source)
+        .enter()
+        .append('path')
+        .attr("d", function (d, i) {
+            return d3.line()
+                .x(function (d) { return margin.left + xScale(d.year); })
+                .y(function (d) { return margin.top + yScale(d.energy); })
+                (d.values)
 
-    const path = svg.append('path')
+        })
+        .attr('fill', 'none')
+        .attr("stroke", function (d) { return color(d.id) })
+        .attr('stroke-width', 2)
+        ;
+
+    /**const path_oil = svg.append('path')
         .datum(data)
         .attr('d', line_oil)
         .attr('fill', 'none')
         .attr('stroke', 'blue')
         .attr('stroke-width', 2);
+
+    const pathLengthOil = path_oil.node().getTotalLength();
+    
+    path_oil
+    .attr("stroke-dashoffset", pathLengthOil)
+    .attr("stroke-dasharray", pathLengthOil)
+    .transition(transitionPath)
+    .attr("stroke-dashoffset", 0);
 
     const pathLength = path.node().getTotalLength();
     
@@ -87,8 +111,8 @@ async function init_energy_cons(svg_width, svg_height, start_year=1800, end_year
     .attr("stroke-dashoffset", pathLength)
     .attr("stroke-dasharray", pathLength)
     .transition(transitionPath)
-    .attr("stroke-dashoffset", 0);
-        
+    .attr("stroke-dashoffset", 0);**/
+
     /**svg.append('g')
         .selectAll('circle')
         .data(data)
@@ -121,31 +145,71 @@ async function init_energy_cons(svg_width, svg_height, start_year=1800, end_year
 
     // Append X-axis
     svg.append('g')
-        .attr("transform",`translate(${margin.left}, ${height - margin.bottom})`)
+        .attr("transform", `translate(${margin.left}, ${height - margin.bottom})`)
         .call(d3.axisBottom(xScale).tickFormat(d3.format('~d')))
 
     // Append Y-axis
     svg.append('g')
-        .attr("transform",`translate(${margin.left}, ${margin.top})`)
+        .attr("transform", `translate(${margin.left}, ${margin.top})`)
         .call(d3.axisLeft(yScale).tickFormat(d3.format('~s')))
 
     // Append x-axis label
     svg.append("text")
-    .attr("y", height)
-    .attr("x", width/2)
-    .style("text-anchor", "middle")
-    .style("fill", "black")
-    .text("Year");
+        .attr("y", height)
+        .attr("x", width / 2)
+        .style("text-anchor", "middle")
+        .style("fill", "black")
+        .text("Year");
 
     // Append y-axis label
     svg.append("text")
-    .attr("transform", "rotate(-90)")
-    .attr("y", 0)
-    .attr("x", -(height/2))
-    .attr("dy", "1em")
-    .style("text-anchor", "middle")
-    .style("fill", "black")
-    .text("Energy Consumption in TWh");
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0)
+        .attr("x", -(height / 2))
+        .attr("dy", "1em")
+        .style("text-anchor", "middle")
+        .style("fill", "black")
+        .text("Energy Consumption in TWh");
+
+    var chartData = source.map(function (d) {
+        console.log(d);
+        return { name: d.id, color: color(d.id) };
+    })
+    console.log(chartData);
+
+    //Initialize legend
+    var legendItemSize = 12;
+    var legendSpacing = 4;
+    var xOffset = 10;
+    var yOffset = 10;
+    var legend = d3
+        .select('#legend')
+        .append('svg')
+        .selectAll('.legendItem')
+        .data(chartData);
+
+    //Create legend items
+    legend
+        .enter()
+        .append('rect')
+        .attr('class', 'legendItem')
+        .attr('width', legendItemSize)
+        .attr('height', legendItemSize)
+        .style('fill', d => d.color)
+        .attr('transform',
+            (d, i) => {
+                var x = xOffset;
+                var y = yOffset + (legendItemSize + legendSpacing) * i;
+                return `translate(${x}, ${y})`;
+            });
+
+    //Create legend labels
+    legend
+        .enter()
+        .append('text')
+        .attr('x', xOffset + legendItemSize + 5)
+        .attr('y', (d, i) => yOffset + (legendItemSize + legendSpacing) * i + 12)
+        .text(d => d.name);
 
     /*const annotations = [{
         type: d3.annotationCalloutElbow,
