88a0ae449f7ac6d385a33bff89b627c7a9419891
CodingWithJS <learningtocodejs@gmail.com>
2023-07-31 03:25:28 +0530

    refactor 2

--- js/charts/co2.js
+++ js/charts/co2.js
@@ -1,4 +1,4 @@
-async function init_c02(svg_width, svg_height, start_year = 1850, end_year = 2020, svg_id = '#global-warming-chart') {
+async function init_co2(svg_width, svg_height, start_year = 1850, end_year = 2020,region='World', svg_id = '#global-warming-chart') {
     const width = svg_width;
     const height = svg_height;
     const margin = { top: 20, right: 20, bottom: 40, left: 60 };
@@ -6,37 +6,33 @@ async function init_c02(svg_width, svg_height, start_year = 1850, end_year = 202
     const chartHeight = height - margin.top - margin.bottom;
     const default_start_year = 1850;
     const default_end_year = 2020;
+    const default_region = 'World';
+    const default_region_dropdown_val = '<option value="' + region + '">' + region + '</option>';
     // Retrieve data
     //const filePath = "././data/co2-emissions-by-fuel-line.csv"
     let raw_data = await d3.csv("././data/co2-emissions-by-fuel-line.csv")
     // Parse the data into appropriate types
     columns = raw_data.columns;
-    data = raw_data.filter(d => d.entity=='World').filter(d => Number(d.year) >= start_year).filter(d => Number(d.year) <= end_year);
+    data = raw_data.filter(d => d.entity == region).filter(d => Number(d.year) >= start_year).filter(d => Number(d.year) <= end_year);
 
     data.forEach(d => {
         d.year = Number(d.year);
+        d.coal = Number(d.coal)/1000000000;
+        d.oil = Number(d.oil)/1000000000;
+        d.gas = Number(d.gas)/1000000000;
+        d.cement = Number(d.cement)/1000000000;
+        d.flaring = Number(d.flaring)/1000000000;
+        d.others = Number(d.others)/1000000000;
     });
 
     var source = columns.slice(3).map(function (id) {
         return {
             id: id,
             values: data.map(function (d) {
-                return { year: d.year, co2: Number(d[id])/1000000000 };
+                return { year: d.year, co2: Number(d[id]) };
             })
         };
     });
-
-
-    if (start_year !== default_start_year && end_year !== default_end_year) {
-        console.log("Please reset chart!")
-    } else if (end_year !== default_end_year) {
-        populateDropdownFilterEndYear('start-years', data);
-    } else if (start_year !== default_start_year) {
-        populateDropdownFilterStartYear('end-years', data);
-    } else {
-        populateDropdownFullYear('start-years', data);
-        populateDropdownFullYear('end-years', data);
-    }
 
     // Create the SVG element
     const svg = d3
@@ -57,9 +53,19 @@ async function init_c02(svg_width, svg_height, start_year = 1850, end_year = 202
         .range([0, chartWidth])
 
     // Create a scale for the y-axis
+        var list = [];
+        data.forEach(d => {
+            list.push(Number(d.coal));
+            list.push(Number(d.oil));
+            list.push(Number(d.gas));
+            list.push(Number(d.cement));
+            list.push(Number(d.flaring));
+            list.push(Number(d.others));
+        })
+    console.log(list);
     const yScale = d3
     .scaleLinear()
-    .domain([0, 20])
+    .domain(d3.extent(list))
     .range([chartHeight, 0]);
 
     // color palette
@@ -67,7 +73,7 @@ async function init_c02(svg_width, svg_height, start_year = 1850, end_year = 202
     var res = ["coal","gas","oil","cement","flaring","others"];
     var color = d3.scaleOrdinal()
             .domain(res)
-            .range(['#e41a1c', '#377eb8', '#4daf4a', '#984ea3', '#ff7f00', '#ffff33'])
+            .range(['#e41a1c', '#377eb8', '#4daf4a', '#984ea3', '#f781bf', '#ffff33'])
 
     // Append the line path
     const path = svg.selectAll(".line")
@@ -109,36 +115,6 @@ async function init_c02(svg_width, svg_height, start_year = 1850, end_year = 202
     .transition(transitionPath)
     .attr("stroke-dashoffset", 0);**/
 
-    /**svg.append('g')
-        .selectAll('circle')
-        .data(data)
-        .enter()
-        .append('circle')
-            .attr('cx', (d) => margin.left + xScale(d.year))
-            .attr('cy', (d) => margin.top + yScale(d.population))
-            .attr('r', 5)
-            .style('fill', 'steelblue')
-            .style('stroke', 'black')
-            .on("mouseover", (d) => {
-                // Show the tooltip on mouseover
-                const tooltip = svg.append("g")
-                .attr("class", "tooltip")
-                .attr("transform", "translate(" + (xScale(d.year) + 10) + "," + (yScale(d.population) - 20) + ")");
-
-                tooltip.append("text")
-                    .attr("y", 15)
-                    .text("Year: " + d.year);
-
-                tooltip.append("text")
-                    .attr("y", 30)
-                    .text("Population: " + d.population);
-            })
-            .on("mouseout", (d,i) => {
-                // Remove the tooltip on mouseout
-                svg.select(".tooltip").remove();
-            })
-            **/
-
     // Append X-axis
     svg.append('g')
         .attr("transform", `translate(${margin.left}, ${height - margin.bottom})`)
@@ -147,7 +123,7 @@ async function init_c02(svg_width, svg_height, start_year = 1850, end_year = 202
     // Append Y-axis
     svg.append('g')
         .attr("transform", `translate(${margin.left}, ${margin.top})`)
-        .call(d3.axisLeft(yScale).tickFormat(d3.format("~s")));
+        .call(d3.axisLeft(yScale).tickFormat(d3.format(".2f")));
 
     // Append x-axis label
     svg.append("text")
@@ -165,7 +141,7 @@ async function init_c02(svg_width, svg_height, start_year = 1850, end_year = 202
         .attr("dy", "1em")
         .style("text-anchor", "middle")
         .style("fill", "black")
-        .text("CO2 emissions");
+        .text("CO2 emissions (Billion Tonnes)");
 
     var chartData = source.map(function (d) {
         return { name: d.id, color: color(d.id) };
@@ -205,24 +181,43 @@ async function init_c02(svg_width, svg_height, start_year = 1850, end_year = 202
         .attr('y', (d, i) => yOffset + (legendItemSize + legendSpacing) * i + 12)
         .text(d => d.name);
 
-    /*const annotations = [{
-        type: d3.annotationCalloutElbow,
+    const annotations = [{
+        type: d3.annotationCalloutCircle,
         connector: { end: "arrow" },
         note: { 
-            label: "resulted in 5,000–175,000 deaths, spread through contact with foreigners",
-            title: "Oku‘u (pestilence)",
-            wrap: 300
+            label: "Fossil fuels being used for energy generation has led to unprecendeted levels of CO2 emissions which is one of the main greenhouse gases",
+            title: "Fossil Fuels Producing Highest CO2 Emissions",
+            wrap: 400
         },
-        x: xScale(1804) + margin.left,
-        y: yScale(210000) + margin.top,
-        dx: 127,
-        dy: -50,
-    }].map(function(d){ d.color = "#E8336D"; return d})
+        subject: {
+            radius: 50,         // circle radius
+            radiusPadding: 0   // white space around circle befor connector
+          },
+        x: xScale(2010) + margin.left,
+        y: yScale(12) + margin.top,
+        dx: -127,
+        dy: -5,
+    },{
+        type: d3.annotationXYThreshold,
+        note: {
+            title: "Kyoto Protocol",
+            label: "In 1997, first agreement between nations to mandate the reduction of greenhouse gases was taken - to reduce emissions by 5% by 2008-12",
+            wrap: 250
+          },
+        subject: {
+            y1: yScale(0) + margin.top,
+            y2: yScale(20) + margin.top,
+        },
+        x: xScale(1997) + margin.left,
+        y: yScale(6) + margin.top,
+        dy: -15,
+        dx: -170,
+    }].map(function(d){ d.color = "#FF4500"; return d})
 
       const makeAnnotations = d3.annotation()
         .annotations(annotations)
 
     svg.append("g")
     .attr("class", "annotation-group")
-    .call(makeAnnotations)*/
+    .call(makeAnnotations)
 }
